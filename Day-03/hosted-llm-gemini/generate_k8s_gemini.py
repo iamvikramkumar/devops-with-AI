dbde18d902bbcfc95e113957761da3f66ca6a3e7
iamvikramkumar <iamvikramkumar5@gmail.com>
2025-03-21 02:26:32 +0530

    generate-k8s-hosted-llm

---
+++ Day-03/hosted-llm-gemini/generate_k8s_gemini.py
@@ -0,0 +1,35 @@
+import google.generativeai as genai
+import os
+
+from grpc import services
+
+# Set your API key here
+os.environ["GOOGLE_API_KEY"] = "AIzaSyAmIjvy-96u0MlJp0u39r0X7wODz_ajb24"
+
+# Configure the Gemini model
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel('gemini-1.5-pro')
+
+PROMPT = """
+Generate an ideal Kubernetes YAML file for deploying a {service_type} application with best practices. Just share the YAML file without any explanation between two lines to make copying easy.
+Include:
+- Namespace
+- ConfigMap for environment variables
+- Secrets for sensitive data
+- Deployment with readiness and liveness probes
+- Horizontal Pod Autoscaler (HPA)
+- Service of type {service_type}
+- Ingress (if applicable) for external access
+"""
+
+
+def generate_k8s(service_type):
+    response = model.generate_content(PROMPT.format(service_type=service_type))
+    return response.text
+
+if __name__ == '__main__':
+    service = input("Please enter the service type (e.g., ClusterIP, NodePort, LoadBalancer): ")
+    kubernetes = generate_k8s(service)
+    print("\nGenerated Kubernetes:\n")
+    print(kubernetes)
+
